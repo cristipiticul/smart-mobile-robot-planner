f0c0d9813227107b964e5236a18519e281109fed
Cristian Militaru <cristipiticul@gmail.com>
2018-07-18 23:16:26 +0300

    Added obstacles in Julia (not finished & not tested)

--- pomdp_planner/julia/turtlebot_grid_world.jl
+++ pomdp_planner/julia/turtlebot_grid_world.jl
@@ -2,8 +2,8 @@ using POMDPs
 importall POMDPs
 using Distributions
 using POMDPToolbox
-include("turtlebot_communication.jl")
-resetGazeboWorld()
+#include("turtlebot_communication.jl")
+#resetGazeboWorld()
 
 # =================================
 # (PO)MDP structures
@@ -13,25 +13,38 @@ struct GridCell
     y::Int64
 end
 
-struct GridWorldState
+# abstract type AgentState end
+
+struct RobotState # <: AgentState
     position::GridCell
     orientation::Symbol # one of [:north, :south, :east, :west]
-    done::Bool # are we in a terminal state?
+end
+
+struct ObstacleState # <: AgentState
+    position::GridCell
+    orientation::Symbol # one of [:north, :south]
+end
+
+struct GridWorldState
+    robot::RobotState
+    obstacles::Vector{ObstacleState}
 end
 
 type GridWorld <: MDP{GridWorldState, Symbol} # Note that our MDP is parametarized by the state and the action
     size_x::Int64 # x size of the grid
     size_y::Int64 # y size of the grid
-    reward_states::Vector{GridCell} # the states in which agent recieves reward
-    reward_values::Vector{Float64} # reward values for those states
-    discount_factor::Float64 # disocunt factor
+    goal::GridCell
+    obstacle_1_x::Int64
+    obstacle_2_x::Int64
+    goal_reward::Float64      # positive, received when robot reaches goal
+    collision_reward::Float64 # negative
+    time_reward::Float64      # negative, to encourage fastest route
+    discount_factor::Float64  # disocunt factor
 end
 
 # =================================
 # Useful functions
 # =================================
-
-GridWorldState(x::Int64, y::Int64, orientation::Symbol) = GridWorldState(GridCell(x, y), orientation, false)
 
 function inbounds(mdp::GridWorld, pos::GridCell)
     return 1 <= pos.x <= mdp.size_x && 1 <= pos.y <= mdp.size_y
@@ -81,23 +94,34 @@ end
 
 posequal(s1::GridCell, s2::GridCell) = s1.x == s2.x && s1.y == s2.y
 
+function stateInCollision(state::GridWorldState)
+    for obstacle = state.obstacles
+        if posequal(state.robot.position, obstacle.position)
+            return true
+        end
+    end
+    return false
+end
+
+function goalIsReached(mdp::GridWorld, state::GridWorldState)
+    return posequal(state.robot.position, mdp.goal)
+end
+
 # =================================
 # (PO)MDP description
 # =================================
 
-# we use key worded arguments so we can change any of the values we pass in
-function GridWorld(;sx::Int64=10, # size_x
-                    sy::Int64=10, # size_y
-                    rs::Vector{GridCell}=[GridCell(4,3), GridCell(4,6), GridCell(9,3), GridCell(8,8)], # reward states
-                    rv::Vector{Float64}=rv = [-10.,-5,10,3], # reward values
-                    discount_factor::Float64=0.9)
-    return GridWorld(sx, sy, rs, rv, discount_factor)
-end
-
 function POMDPs.states(mdp::GridWorld)
     s = GridWorldState[]
-    for d = 0:1, y = 1:mdp.size_y, x = 1:mdp.size_x, orientation = [:north, :south, :east, :west]
-        push!(s, GridWorldState(GridCell(x,y),orientation,d))
+    for robot_y = 1:mdp.size_y, robot_x = 1:mdp.size_x, robot_orientation = [:north, :south, :east, :west],
+        obstacle_1_y = 1:mdp.size_y, obstacle_1_orientation = [:east, :west],
+        obstacle_2_y = 1:mdp.size_y, obstacle_2_orientation = [:east, :west]
+
+        robot_state = RobotState(GridCell(robot_x, robot_y), robot_orientation)
+        obstacle_1_state = ObstacleState(GridCell(mdp.obstacle_1_x, obstacle_1_y), obstacle_1_orientation)
+        obstacle_2_state = ObstacleState(GridCell(mdp.obstacle_2_x, obstacle_2_y), obstacle_2_orientation)
+
+        push!(s, GridWorldState(robot_state, [obstacle_1_state, obstacle_2_state]))
     end
     return s
 end
@@ -105,43 +129,42 @@ end
 POMDPs.actions(mdp::GridWorld) = [:forward, :rotate_clockwise, :rotate_counterclockwise, :wait]
 
 function POMDPs.transition(mdp::GridWorld, state::GridWorldState, action::Symbol)
-    if state.done
-        return SparseCat([GridWorldState(state.position, state.orientation, true)], [1.0])
-    elseif state.position in mdp.reward_states
-        return SparseCat([GridWorldState(state.position, state.orientation, true)], [1.0])
+    next_obstacles_states = Vector{ObstacleState}()
+    for obstacle = state.obstacles
+        # TODO: the obstacle turns at the end of the way
+        new_position = forwardPosition(obstacle.position, obstacle.orientation)
+        new_orientation = obstacle.orientation
+        push!(next_obstacles_states, ObstacleState(new_position, new_orientation))
     end
 
-    next_position::GridCell = state.position
-    next_orientation::Symbol = state.orientation
+    next_robot_position::GridCell = state.robot.position
+    next_robot_orientation::Symbol = state.robot.orientation
     if action == :rotate_clockwise
-        next_orientation = rotateClockwise(state.orientation)
+        next_robot_orientation = rotateClockwise(state.robot.orientation)
     elseif action == :rotate_counterclockwise
-        next_orientation = rotateCounterClockwise(state.orientation)
+        next_robot_orientation = rotateCounterClockwise(state.robot.orientation)
     elseif action == :forward
-        new_position = forwardPosition(state.position, state.orientation)
-        if inbounds(mdp, new_position)
-            next_position = new_position
+        forward_robot_position = forwardPosition(state.robot.position, state.robot.orientation)
+        if inbounds(mdp, forward_robot_position)
+            next_robot_position = forward_robot_position
         end
     elseif action == :wait
         # do nothing, they are already initialized
     else
         println("Error! Unrecognized action: $action")
     end
-    return SparseCat([GridWorldState(next_position, next_orientation, false)], [1.0])
+    next_robot_state = RobotState(next_robot_position, next_robot_orientation)
+    return SparseCat([GridWorldState(next_robot_state, next_obstacles_states)], [1.0])
 end
 
-function POMDPs.reward(mdp::GridWorld, state::GridWorldState, action::Symbol, statep::GridWorldState) #deleted action
-    if state.done
-        return 0.0
+function POMDPs.reward(mdp::GridWorld, state::GridWorldState, action::Symbol, statep::GridWorldState)
+    if stateInCollision(state)
+        return mdp.collision_reward
+    elseif goalIsReached(mdp, state)
+        return mdp.goal_reward
+    else
+        return mdp.time_reward
     end
-    r = -1.0
-    n = length(mdp.reward_states)
-    for i = 1:n
-        if posequal(state.position, mdp.reward_states[i])
-            r += mdp.reward_values[i]
-        end
-    end
-    return r
 end
 
 
@@ -149,16 +172,27 @@ end
 # done: 0 or 1
 # x between 1, mdp.size_x
 # y between 1, mdp.size_y
-POMDPs.n_states(mdp::GridWorld) = 4*2*mdp.size_x*mdp.size_y
-# forward, rotate_clockwise, rotate_counterclockwise
-POMDPs.n_actions(mdp::GridWorld) = 3
+POMDPs.n_states(mdp::GridWorld) = mdp.size_x * mdp.size_y * 4 *
+                                  mdp.size_y * 2 *
+                                  mdp.size_y * 2
+# forward, rotate_clockwise, rotate_counterclockwise, wait
+POMDPs.n_actions(mdp::GridWorld) = 4
 POMDPs.discount(mdp::GridWorld) = mdp.discount_factor;
 
 function POMDPs.state_index(mdp::GridWorld, state::GridWorldState)
-    sd = Int(state.done + 1)
-    orientations = Dict(:north=>1, :south=>2, :east=>3, :west=>4)
-    orientation = orientations[state.orientation]
-    return sub2ind((mdp.size_x, mdp.size_y, 4, 2), state.position.x, state.position.y, orientation, sd)
+    orientations_all = Dict(:north=>1, :south=>2, :east=>3, :west=>4)
+    orientations_two = Dict(:north=>1, :south=>2)
+    robot_orientation = orientations_all[state.robot.orientation]
+    obstacle_1_orientation = orientations_two[state.obstacle_1.orientation]
+    obstacle_2_orientation = orientations_two[state.obstacle_2.orientation]
+    return sub2ind(
+        (mdp.size_x, mdp.size_y, 4,
+        mdp.size_y, 2,
+        mdp.size_y, 2),
+        (state.robot.position.x, state.robot.position.y, robot_orientation,
+        state.obstacle_1.position.y, obstacle_1_orientation,
+        state.obstacle_2.position.y, obstacle_2_orientation)
+    )
 end
 
 function POMDPs.action_index(mdp::GridWorld, act::Symbol)
@@ -174,14 +208,24 @@ function POMDPs.action_index(mdp::GridWorld, act::Symbol)
     error("Invalid GridWorld action: $act")
 end
 
-POMDPs.isterminal(mdp::GridWorld, s::GridWorldState) = s.done
+POMDPs.isterminal(mdp::GridWorld, s::GridWorldState) = stateInCollision(s) || goalIsReached(mdp, s)
 
 # =================================
 # (PO)MDP solving
 # =================================
 using MCTS
 
-mdp = GridWorld()
+mdp = GridWorld(
+    10,              #size_x::Int64
+    10,              #size_y::Int64
+    GridCell(9, 5),  #goal::GridCell
+    4,               #obstacle_1_x::Int64
+    6,               #obstacle_2_x::Int64
+    100,             #goal_reward::Float64
+    -400,            #collision_reward::Float64
+    -5,              #time_reward::Float64
+    0.9             #discount_factor::Float64
+)
 
 # initialize the solver with hyper parameters
 # n_iterations: the number of iterations that each search runs for
@@ -198,15 +242,21 @@ policy = solve(solver, mdp)
 # =================================
 # Test the policy
 # =================================
-start_state = GridWorldState(3, 6, :south)
+start_state = GridWorldState(
+    RobotState(GridCell(3, 6), :south),
+    [
+        ObstacleState(GridCell(mdp.obstacle_1_x, 1), :north),
+        ObstacleState(GridCell(mdp.obstacle_2_x, 6), :south)
+    ]
+)
 
 sim(mdp, start_state, max_steps=20) do s
     println("State: $s")
     a = action(policy, s)
     println("Action: $a")
-    if !robotDoAction(a)
-        println("Robot failed to do action: $a")
-        return :wait
-    end
+    #if !robotDoAction(a)
+    #    println("Robot failed to do action: $a")
+    #    return :wait
+    #end
     return a
 end
